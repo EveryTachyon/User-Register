24f3b53f12c29a31d9a52a3972f93d8a033acd20
Every <marken.mangelsoo@tptlive.ee>
2026-03-21 08:52:31 +0200

    mock api test

--- app.mock.test.js
+++ app.mock.test.js
@@ -1,23 +1,17 @@
+jest.mock('./validation/validateEmail')
+
 const createApp = require('./app')
 const request = require('supertest')
 const validateUsername = require('./validation/validateUsername')
 const validatePassword = require('./validation/validatePassword')
-
-//Mock validateEmail to isolate tests
-jest.mock('./validation/validateEmail', () => {
-    return jest.fn((email) => {
-        //Simulate real world simulation
-        if (!email || typeof email !== 'string') return false;
-        const re = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
-        return re.test(email);
-    })
-})
-
 const validateEmail = require('./validation/validateEmail')
+
 const app = createApp(validateUsername, validatePassword, validateEmail)
 
 describe('given correct username and password', () => {
     test('return status 200', async () => {
+        validateEmail.mockReturnValue(true)
+
         const response = await request(app).post('/users').send({
             username: 'Username',
             password: 'Password123',
@@ -27,6 +21,8 @@ describe('given correct username and password', () => {
     })
 
     test('returns userId', async () => {
+        validateEmail.mockReturnValue(true)
+
         const response = await request(app).post('/users').send({
             username: 'Username',
             password: 'Password123',
@@ -34,15 +30,12 @@ describe('given correct username and password', () => {
         })
         expect(response.body.userId).toBeDefined();
     })
-
-    // test response content type?
-    // test response message
-    // test response user id value
-    // ...
 })
 
 describe('given incorrect or missing username and password', () => {
     test('return status 400', async () => {
+        validateEmail.mockReturnValue(false)
+
         const response = await request(app).post('/users').send({
             username: 'user',
             password: 'password',
@@ -50,10 +43,4 @@ describe('given incorrect or missing username and password', () => {
         })
         expect(response.statusCode).toBe(400)
     })
-
-    // test response message
-    // test that response does NOT have userId
-    // test incorrect username or password according to requirements
-    // test missing username or password
-    // ...
 })
